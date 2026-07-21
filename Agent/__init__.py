c0149b9c4b1e9eed4ff71f9b45a7d8c3125d6a01
23viv <vivekkapri5@gmail.com>
2026-07-21 20:48:05 +0530

    include PET_ROUTES

--- Agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from Agent.vet_agent import run_vet_agent
-
-__all__ = ["run_vet_agent"]
--- main_route.py
+++ main_route.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routes.Auth import auth_routes
 from routes.Message import Message_route
+from routes.Pets import Pets_routes
 
 app = FastAPI()
 
 app.include_router(auth_routes.router)
 app.include_router(Message_route.router)
+app.include_router(Pets_routes.router)
